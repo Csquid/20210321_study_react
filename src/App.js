2616d3600a02a5004e05de7b8fd4d146d6116b16
Csquid <rmaqlctnekf@naver.com>
2021-03-25 21:34:07 +0900

    add event

--- src/App.js
+++ src/App.js
@@ -1,29 +1,66 @@
 import React, { Component } from 'react';
 import Subject from './Components/Subject';
 import List from './Components/List';
-import Content from './Components/Content';
+import ReadContent from './Components/ReadContent';
+import Controll from './Components/Controll';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      subject: { title: "Web", comment: "World wide web!"},
+      mode: 'read',
+      listIDX: 0,
+      subject: { title: "Web",      description: "World wide web!" },
+      welcome: { title: "Welcome",  description: "Hello, React!!"},
       lists: [
-        {id: 1, title: 'HTML', description: 'HTML is for information'},
-        {id: 2, title: 'CSS', description: 'CSS is for design'},
-        {id: 3, title: 'JavaScript', description: 'JavaScript is for information'}
+        {id: 0, title: 'HTML',        description: 'HTML is for information'},
+        {id: 1, title: 'CSS',         description: 'CSS is for design'},
+        {id: 2, title: 'JavaScript',  description: 'JavaScript is for information'}
       ]
-    };
+    }
   }
+      
   render() {
+    console.log('App render');
+    let _title, _description = null;
+    if(this.state.mode === 'welcome') {
+      _title        = this.state.welcome.title;
+      _description  = this.state.welcome.description;
+    } else if (this.state.mode === 'read') {
+      _title        = this.state.lists[this.state.listIDX].title;
+      _description  = this.state.lists[this.state.listIDX].description;
+    }
+    
     return (
       <div className="App">
-        <Subject title={this.state.subject.title} comment={this.state.subject.comment}></Subject>
-        <List data={this.state.lists}></List>
-        <Content></Content>
+        <Subject 
+          title={this.state.subject.title} 
+          description={this.state.subject.description}
+          // custom
+          onChangePage={() => {
+            this.setState({mode: "welcome"});
+          }}
+        ></Subject>
+        <List 
+          data={this.state.lists}
+          onChangePage={(idx) => {
+            this.setState({
+              mode: 'read',
+              listIDX: idx
+            });
+          }}
+        >
+        </List>
+        <Controll onChangePage={(nMode) => {
+          this.setState({
+            mode: nMode
+          })
+        }}></Controll>
+        <ReadContent title={_title} description={_description}></ReadContent>
       </div>
     );
   }
 }
 
 export default App;
+//2018.03.25
